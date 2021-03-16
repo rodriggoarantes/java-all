d1a378c0a362930aeacc6df2815a8c43bdd1805b
rodrigo.arantes <rodrigo.arantes@totvs.com.br>
2021-03-16 17:18:24 -0300

    ordenaçao de conjuntos sets

--- src/main/java/br/com/ras/java/collections/Main.java
+++ src/main/java/br/com/ras/java/collections/Main.java
@@ -2,9 +2,7 @@ package br.com.ras.java.collections;
 
 import br.com.ras.java.shared.LogUtil;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -14,6 +12,8 @@ public class Main extends LogUtil {
     public static void main(String[] args) {
         interateList();
         sortList();
+
+        sortSet();
     }
 
     private static void interateList() {
@@ -63,13 +63,76 @@ public class Main extends LogUtil {
         lista.stream().sorted().forEach(Main::outln);
     }
 
-    private static class Element {
+
+    private static void sortSet() {
+        final var valores = Set.of(Element.of("1", 2),
+                                   Element.of("9", 9),
+                                   Element.of("5", 5),
+                                   Element.of("2", 2),
+                                   Element.of("1", 1));
+
+        outln("----- list sort -----");
+        final var list = new ArrayList<>(valores);
+        list.sort(Comparator.comparing(e -> e.id));
+        list.forEach(e -> outln(e.toString()));
+
+        outln("----- tree sort -----");
+        final var arvoreValores = unmodifiableSorted(valores);
+        arvoreValores.forEach(e -> outln(e.toString()));
+        outln("-----");
+    }
+
+    private static Set<Element> unmodifiableSorted(Set<Element> valores) {
+        final var nullSafeSet = unmodifiable(valores);
+        final var orderedList = new ArrayList<>(nullSafeSet);
+        Collections.sort(orderedList);
+        return new TreeSet<>(orderedList);
+    }
+
+    public static <T> Set<T> unmodifiable(Set<T> collection) {
+        return collection == null ? Collections.emptySet() : Collections.unmodifiableSet(collection);
+    }
+
+    private static class Element implements Comparable<Element> {
         private final String id;
         private final Integer value;
+
+        public static Element of(String id, Integer value) {
+            return new Element(id, value);
+        }
 
         public Element(String id, Integer value) {
             this.id = id;
             this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Element)) return false;
+            Element element = (Element) o;
+            return Objects.equals(id, element.id)
+                    && Objects.equals(value, element.value);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, value);
+        }
+
+        @Override
+        public String toString() {
+            return "Element{" +
+                    "id='" + id + '\'' +
+                    ", value=" + value +
+                    '}';
+        }
+
+        @Override
+        public int compareTo(Element o) {
+            final var compareId = this.id.compareTo(o.id);
+            if (compareId != 0) return compareId;
+            return this.value.compareTo(o.value);
         }
     }
 
