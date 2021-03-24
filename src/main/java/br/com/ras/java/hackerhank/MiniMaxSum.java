0827cc7eaaead40a19495e6924d8cd50f65112ab
rodrigo.arantes <rodrigo.arantes@totvs.com.br>
2021-03-24 17:33:17 -0300

    min max sum

---
+++ src/main/java/br/com/ras/java/hackerhank/MiniMaxSum.java
@@ -0,0 +1,65 @@
+package br.com.ras.java.hackerhank;
+
+public class MiniMaxSum {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 2, 3, 4, 5};
+        miniMaxSum(array);
+        alternative(array);
+    }
+
+    static void miniMaxSum(int[] arr) {
+        long[] result = solution(arr);
+
+        System.out.println(result[0] + " " + result[1]);
+    }
+
+    static long[] solution(int[] arr) {
+        int length = arr.length;
+        long max = 0;
+        long min = 0;
+
+        for (int i=0; i < length; i++) {
+            long minidx = sumall(i, arr);
+            long maxidx = sumall(i, arr);
+
+            if (i == 0) {
+                min = minidx;
+                max = maxidx;
+            }
+            if (minidx < min) {
+                min = minidx;
+            }
+            if (maxidx > max) {
+                max = minidx;
+            }
+        }
+
+        return new long[]{min, max};
+    }
+
+    static long sumall(int ignoreIndex, int[] arr) {
+        long sum = 0;
+        for (int index = 0; index < arr.length; index++) {
+            if (ignoreIndex != index) {
+                sum += arr[index];
+            }
+        }
+        return sum;
+    }
+
+    // ----------------------------------------------------
+
+    static void alternative(int[] arr) {
+        long sum = 0;
+        long max = Long.MIN_VALUE;
+        long min = Long.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            long n = arr[i];
+            sum += n;
+            max = Math.max(max, n);
+            min = Math.min(min, n);
+        }
+        System.out.println((sum - max) + " " + (sum - min));
+    }
+}
---
+++ src/main/java/br/com/ras/java/hackerhank/Staircase.java
@@ -0,0 +1,79 @@
+package br.com.ras.java.hackerhank;
+
+/**
+ * Staircase detail
+ *
+ * This is a staircase of size :
+ *
+ *    #
+ *   ##
+ *  ###
+ * ####
+ * Its base and height are both equal to . It is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+ *
+ * Write a program that prints a staircase of size .
+ *
+ * Function Description
+ *
+ * Complete the staircase function in the editor below.
+ *
+ * staircase has the following parameter(s):
+ *
+ * int n: an integer
+ * Print
+ *
+ * Print a staircase as described above.
+ *
+ * Input Format
+ *
+ * A single integer, , denoting the size of the staircase.
+ *
+ * Constraints
+ *
+ *  .
+ *
+ * Output Format
+ *
+ * Print a staircase of size  using # symbols and spaces.
+ *
+ * Note: The last line must have  spaces in it.
+ *
+ * Sample Input
+ *
+ * 6
+ * Sample Output
+ *
+ *      #
+ *     ##
+ *    ###
+ *   ####
+ *  #####
+ * ######
+ * Explanation
+ *
+ * The staircase is right-aligned, composed of # symbols and spaces, and has a height and width of n=6.
+ */
+public class Staircase {
+
+    public static void main(String[] args) {
+        staircase(6);
+    }
+
+    static void staircase(int n) {
+        int length = n - 1;
+
+        for (int i=0; i < n; i++) {
+            int blank = length - i;
+
+            StringBuilder line = new StringBuilder();
+            for (int j=0; j < n; j++) {
+                if (j < blank) {
+                    line.append(" ");
+                } else {
+                    line.append("#");
+                }
+            }
+            System.out.println(line.toString());
+        }
+    }
+}
